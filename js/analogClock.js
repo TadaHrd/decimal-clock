28404ffb3c0596698e783bb4e4c76a827045ca0f
tadaHrd <hrudka.tadeas@gmail.com>
2024-05-10 21:21:28 +0200

    fixed hand rotations and such

--- js/analogClock.js
+++ js/analogClock.js
@@ -12,6 +12,6 @@ function setTime() {
     let time = normalSecondsToDecimalTime(getAllDaySeconds(date, true));
 
     secondsHand.setAttribute("transform", `rotate(${time.secondsMilliseconds * 3.6 - 61 /* default rotation of hand */} 50 50)`);
-    minutesHand.setAttribute("transform", `rotate(${time.minutes * 3.6 - 34 /* default rotation of hand */} 50 50)`);
-    hoursHand.setAttribute("transform", `rotate(${(time.hours + time.minutes / 60) * 45 - 14 /* default rotation of hand */} 50 50)`);
+    minutesHand.setAttribute("transform", `rotate(${(time.minutes + time.seconds / 100) * 3.6 - 36 /* default rotation of hand */} 50 50)`);
+    hoursHand.setAttribute("transform", `rotate(${(time.hours + time.minutes / 100) * 45 - 16 /* default rotation of hand */} 50 50)`);
 }
